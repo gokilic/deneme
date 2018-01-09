425e908a5ad6a5871358e341d4659b74fd3afed0
Vlad Gheorghiu <vsoftco@gmail.com>
2018-01-09 10:41:06 -0500

    commit

--- src/common/oqs.h
+++ src/common/oqs.h
@@ -9,28 +9,41 @@
 #include <stdio.h>
 #include <stdint.h>
 
+/**
+ * \brief Displays hexadecimal strings
+ * \param label Label displayed before the hexadecimal string
+ * \param str The hexadecimal string
+ * \param len The length (in bytes) of the hexadecimal string
+ */
 static inline void print_hex_string(const char* label, uint8_t* str, size_t len)
 {
-    printf("%-20s (%4zu bytes):  ", (label), (size_t)(len));
+    printf("%-20s (%4zu bytes):  ", label, len);
     for (size_t i = 0; i < (len); i++)
     {
-        printf("%02X", ((unsigned char *)(str))[i]);
+        printf("%02X", ((unsigned char*) (str))[i]);
     }
     printf("\n");
 }
 
+/**
+* \brief Partially displays hexadecimal strings
+* \param label Label displayed before the hexadecimal string
+* \param str The hexadecimal string
+* \param len The length (in bytes) of the full hexadecimal string
+* \param sub_len How many bytes of the full hexadecimal string to be displayed
+*/
 static inline void print_partial_hex_string(const char* label, uint8_t* str,
-                                     size_t len, size_t sub_len)
+                                            size_t len, size_t sub_len)
 {
-    printf("%-20s (%4zu bytes):  ", (label), (size_t)(len));
+    printf("%-20s (%4zu bytes):  ", label, len);
     for (size_t i = 0; i < (sub_len); i++)
     {
-        printf("%02X", ((unsigned char *)(str))[i]);
+        printf("%02X", ((unsigned char*) (str))[i]);
     }
     printf("...");
     for (size_t i = 0; i < (sub_len); i++)
     {
-        printf("%02X", ((unsigned char *)(str))[len - sub_len + i]);
+        printf("%02X", ((unsigned char*) (str))[len - sub_len + i]);
     }
     printf("\n");
 }
